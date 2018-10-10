bb98b388cc18fccd1504cc0fcf7ca834dc5e8510
CESDE <CESDE@PC-27>
2018-10-10 09:14:09 -0500

    ..

--- AplicacionPrueba/src/aplicacionprueba/AplicacionPrueba.java
+++ AplicacionPrueba/src/aplicacionprueba/AplicacionPrueba.java
@@ -16,9 +16,11 @@ public class AplicacionPrueba {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        // modificado por luis
         
         // modificando el codigo fuente de mi aplicacion
-        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo cruel ");
+     
     }
     
 }
